3a4f8622fabaf29674f0ac1961c9aba440f18228
Felix Dahmen <Felix@echoes.xyz>
2016-11-21 13:58:41 +0700

    Edited Example

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ extern "C" int fast9_asm(uint8_t *img,uint32_t *kps,uint16_t *paras);
 #define HT 240
 #define WT 320
 #define KPS_CNT 5600
+#define THRESHOLD 8
 typedef std::chrono::high_resolution_clock clock2;
 typedef std::chrono::microseconds res;
 struct MY_Keypoint{
@@ -24,14 +25,12 @@ int main() {
 	}
 	MY_Keypoint kps[KPS_CNT];
 	uint32_t *kpP=(uint32_t *)&kps;
-	uint16_t paras[2]={240,KPS_CNT};
+	uint16_t paras[3]={240,KPS_CNT,THRESHOLD};
 	t1 = clock2::now();
-	fast9_asm(img,kpP,paras);
+	uint32_t* kps_cnt2=fast9_asm(img,kpP,paras);
 	t2 = clock2::now();
-	for(int i=0;i<KPS_CNT;i++){
-		if(kps[i].x!=0||kps[i].y!=0){
-			cout<<"kps: "<<kps[i].x<<" "<<kps[i].y<<endl;
-		}
+	for(int i=0;i<*kps_cnt2;i++){
+		cout<<"kps: "<<kps[i].x<<" "<<kps[i].y<<endl;
 	}
 	std::cout << "Elapsed time is \n" << std::chrono::duration_cast<res>((t2-t1)).count()<< " microseconds.\n";
 	return 0;
